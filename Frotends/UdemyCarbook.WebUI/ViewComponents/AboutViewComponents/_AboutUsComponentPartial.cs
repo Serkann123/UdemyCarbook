fbfc0ec0023416f006d87737cbc69f2a29dfdef9
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2024-11-12 20:04:35 +0300

    .

---
+++ Frotends/UdemyCarbook.WebUI/ViewComponents/AboutViewComponents/_AboutUsComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarbook.Dto.AboutDtos;
+
+namespace UdemyCarbook.WebUI.ViewComponents.AboutViewComponents
+{
+    public class _AboutUsComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _HttpClientFactory;
+
+        public _AboutUsComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _HttpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _HttpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync("https://localhost:7126/api/Abouts");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frotends/UdemyCarbook.WebUI/ViewComponents/UIViewComponents/_HeaderUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarbook.WebUI.ViewComponents.UIViewComponents
+{
+    public class _HeaderUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
