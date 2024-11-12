4fca45e2cf72572265ff69109afd95eb5f748dd4
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2024-11-12 20:04:35 +0300

    UI ve DTO katmanları eklendi

---
+++ Frotends/UdemyCarbook.WebUI/ViewComponents/UIViewComponents/_FooterUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyCarbook.WebUI.ViewComponents.UIViewComponents
+{
+    public class _FooterUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
