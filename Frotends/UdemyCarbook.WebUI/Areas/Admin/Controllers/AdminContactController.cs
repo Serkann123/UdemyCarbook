af302b90d83177ca463cc199b2a3bb152e649480
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2024-12-31 22:24:07 +0300

    .

---
+++ Frotends/UdemyCarbook.WebUI/Areas/Admin/Controllers/AdminContactController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UdemyCarbook.Dto.CreateContactDtos;
+
+namespace UdemyCarbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminContact")]
+    public class AdminContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync("https://localhost:7126/api/Contacts");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("RemoveContact/{id}")]
+        public async Task<IActionResult> RemoveContact(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.DeleteAsync($"https://localhost:7126/api/Contacts/{id}");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
