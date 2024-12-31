3b8b25a0e32b34d6bd5bcda6454e445229e35292
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2024-12-31 22:24:07 +0300

    .

---
+++ Frotends/UdemyCarbook.WebUI/Areas/Admin/Controllers/AdminFooterAddressController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UdemyCarbook.Dto.FooterAdressDtos;
+
+namespace UdemyCarbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminFooterAddress")]
+    public class AdminFooterAddressController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminFooterAddressController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync("https://localhost:7126/api/FooterAddress");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        [Route("CreateFooterAddress")]
+        public ActionResult CreateFooterAddress()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateFooterAddress")]
+        public async Task<IActionResult> CreateFooterAddress(CreateFooterAddressDto createFooterAddressDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createFooterAddressDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responsMessage = await client.PostAsync("https://localhost:7126/api/FooterAddress", stringContent);
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminFooterAddress", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("RemoveFooterAddress/{id}")]
+        public async Task<IActionResult> RemoveFooterAddress(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.DeleteAsync($"https://localhost:7126/api/FooterAddress/{id}");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdateFooterAddress/{id}")]
+        public async Task<IActionResult> UpdateFooterAddress(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync($"https://localhost:7126/api/FooterAddress/{id}");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateFooterAddressDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateFooterAddress/{id}")]
+        public async Task<IActionResult> UpdateFooterAddress(UpdateFooterAddressDto updateFooterAddressDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateFooterAddressDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responsMessage = await client.PutAsync("https://localhost:7126/api/FooterAddress/", stringContent);
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminFooterAddress", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
---
+++ Frotends/UdemyCarbook.WebUI/Areas/Admin/Controllers/AdminSocailMediaController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UdemyCarbook.Dto.SocialMediaDtos;
+
+namespace UdemyCarbook.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminSocialMedia")]
+    public class AdminSocialMediaController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminSocialMediaController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync("https://localhost:7126/api/SocialMedias");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateSocialMedia")]
+        public ActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateSocialMedia")]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSocialMediaDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responsMessage = await client.PostAsync("https://localhost:7126/api/SocialMedias", stringContent);
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("RemoveSocialMedia/{id}")]
+        public async Task<IActionResult> RemoveSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.DeleteAsync($"https://localhost:7126/api/SocialMedias/{id}");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("UpdateSocialMedia/{id}")]
+        public async Task<IActionResult> UpdateSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responsMessage = await client.GetAsync($"https://localhost:7126/api/SocialMedias/{id}");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateSocialMediaDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateSocialMedia/{id}")]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateSocialMediaDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responsMessage = await client.PutAsync("https://localhost:7126/api/SocialMedias/", stringContent);
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
