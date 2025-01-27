19706fc5937e0b77bb13f22a26a614c370560ad1
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2025-01-27 17:02:29 +0300

    .

---
+++ Core/UdemyCarbook.Application/Features/Mediator/Handlers/RentACarHandler/GetRentACarQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarbook.Application.Features.Mediator.Queries.RentACarQueiries;
+using UdemyCarbook.Application.Features.Mediator.Results.RentACarResults;
+using UdemyCarbook.Application.Interfaces.RentACarInterfaces;
+
+namespace UdemyCarbook.Application.Features.Mediator.Handlers.RentACarHandler
+{
+    public class GetRentACarQueryHandler:IRequestHandler<GetRentACarQuery,List<GetRentACarQueryResult>>
+    {
+        private readonly IRentACarRepository _rentACarRepository;
+
+        public GetRentACarQueryHandler(IRentACarRepository rentACarRepository)
+        {
+            _rentACarRepository = rentACarRepository;
+        }
+
+        public async Task<List<GetRentACarQueryResult>> Handle(GetRentACarQuery request, CancellationToken cancellationToken)
+        {
+            var values = _rentACarRepository.GetByFilterAsync(x => x.LocationId == request.LocationId && x.Available == true);
+            return values.Select(x => new GetRentACarQueryResult
+            {
+                CarId = x.CarId
+            }).ToList();
+        }
+    }
+}
---
+++ Infastructure/UdemyCarbook.Persistence/Repositories/RentACarRepositories/RentACarRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarbook.Application.Interfaces.RentACarInterfaces;
+using UdemyCarbook.Domain.Entities;
+using UdemyCarbook.Persistence.Context;
+
+namespace UdemyCarbook.Persistence.Repositories.RentACarRepositories
+{
+    public class RentACarRepository:IRentACarRepository
+    {
+        private readonly CarbookContext _carbookContext;
+
+        public RentACarRepository(CarbookContext carbookContext)
+        {
+            _carbookContext = carbookContext;
+        }
+
+        public List<RendACar> GetByFilterAsync(Expression<Func<RendACar, bool>> filter)
+        {
+            var value = _carbookContext.RendACars.Where(filter);
+            return value.ToList();
+        }
+    }
+}
