a512da271937e6d9618cb4effd5fb46a5de7ce4c
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2025-05-01 16:43:53 +0300

    .

---
+++ Core/UdemyCarbook.Application/ViewsModel/BlogListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarbook.Dto.BlogDtos;
+
+namespace UdemyCarbook.Application.ViewsModel
+{
+    public class BlogListViewModel
+    {
+        public List<ResultBlogsAllWithAuthor> Blogs { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
---
+++ Frotends/UdemyCarbook.WebUI/ViewComponents/DashboardComponents/_AdminDashboardBlogListComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using UdemyCarbook.Dto.BlogDtos;
+using X.PagedList.Extensions;
+using X.PagedList;
+namespace UdemyCarbook.WebUI.ViewComponents.DashboardComponents
+{
+    public class _AdminDashboardBlogListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminDashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responsMessage = await client.GetAsync("https://localhost:7126/api/Blog/GetBlogsAllWithAuthorsList");
+            if (responsMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responsMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBlogsAllWithAuthor>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
