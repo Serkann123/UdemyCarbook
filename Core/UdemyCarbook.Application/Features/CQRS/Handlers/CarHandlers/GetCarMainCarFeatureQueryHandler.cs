0425a7766f5af80d1dcb928f9f97342a0c5b7d44
serkanaltunay0203 <serkanaltunay0203@gmail.com>
2025-05-06 02:14:23 +0300

    .

---
+++ Core/UdemyCarbook.Application/Features/CQRS/Handlers/CarHandlers/GetCarMainCarFeatureQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarbook.Application.Features.CQRS.Queries.CarQueries;
+using UdemyCarbook.Application.Features.CQRS.Results.CarResults;
+using UdemyCarbook.Application.Features.Mediator.Queries.BlogQueries;
+using UdemyCarbook.Application.Features.Mediator.Results.BlogResults;
+using UdemyCarbook.Application.Interfaces.CarInterfaces;
+
+namespace UdemyCarbook.Application.Features.CQRS.Handlers.CarHandlers
+{
+    public class GetCarMainCarFeatureQueryHandler
+    {
+        private readonly ICarRepository _repository;
+
+        public GetCarMainCarFeatureQueryHandler(ICarRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCarMainCarFeatureResult> Handle(GetCarMainCarFeatureQuery request, CancellationToken cancellationToken)
+        {
+            var values = _repository.GetCarMainCarFeature(request.Id);
+            return new GetCarMainCarFeatureResult
+            {
+                BigImageUrl = values.BigImageUrl,
+                BrandId = values.BrandId,
+                BrandName = values.Brand.Name,
+                CarId = values.CarId,
+                CoverImageUrl = values.CoverImageUrl,
+                Fuel = values.Fuel,
+                Km = values.Km,
+                Luggage = values.Luggage,
+                Model = values.Model,
+                Seat = values.Seat,
+                Transmission = values.Transmission,
+            };
+        }
+    }
+}
